8c976dff0bd0c9d672aa34f5b13174fdda67e4f8
Benjamin Perche <bperche9@gmail.com>
2015-04-13 13:17:34 +0200

    Prepare version 2.1.3

--- change-version.sh
+++ change-version.sh
@@ -15,13 +15,39 @@ echo "${blue}Downloading composer${NC}"
 curl -sS https://getcomposer.org/installer | php > /dev/null
 
 echo "${blue}Upgrading to $version${NC}"
-php composer.phar require thelia/core $version
-php composer.phar require thelia/setup $version
-php composer.phar require thelia/config $version
-php composer.phar require thelia/frontoffice-default-template $version
-php composer.phar require thelia/backoffice-default-template $version
-php composer.phar require thelia/email-default-template $version
-php composer.phar require thelia/pdf-default-template $version
+php composer.phar require thelia/core                               $version
+php composer.phar require thelia/setup                              $version
+php composer.phar require thelia/config                             $version
+php composer.phar require thelia/frontoffice-default-template       $version
+php composer.phar require thelia/backoffice-default-template        $version
+php composer.phar require thelia/email-default-template             $version
+php composer.phar require thelia/smarty-module                      $version
+php composer.phar require thelia/front-module                       $version
+php composer.phar require thelia/virtual-product-control-module     $version
+php composer.phar require thelia/virtual-product-delivery-module    $version
+php composer.phar require thelia/tinymce-module                     $version
+php composer.phar require thelia/colissimo-module                   $version
+php composer.phar require thelia/cheque-module                      $version
+php composer.phar require thelia/thelia-debug-bar-module            $version
+php composer.phar require thelia/hook-lang-module                   $version
+php composer.phar require thelia/hook-search-module                 $version
+php composer.phar require thelia/hook-currency-module               $version
+php composer.phar require thelia/hook-products-new-module           $version
+php composer.phar require thelia/hook-analytics-module              $version
+php composer.phar require thelia/hook-links-module                  $version
+php composer.phar require thelia/hook-social-module                 $version
+php composer.phar require thelia/hook-cart-module                   $version
+php composer.phar require thelia/hook-navigation-module             $version
+php composer.phar require thelia/hook-contact-module                $version
+php composer.phar require thelia/hook-customer-module               $version
+php composer.phar require thelia/hook-products-offer-module         $version
+php composer.phar require thelia/hook-newsletter-module             $version
+
+if [ $version -ge "2.1.2" ]; then
+php composer.phar require thelia/carousel-module                    $version
+else
+php composer.phar remove thelia/carousel-module
+fi
 
 echo "${blue}Deleting composer${NC}"
 rm -f composer.phar

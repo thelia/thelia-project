e34ea9d39b0071f8398595684c4dd6944fc1f193
Alexandre Nozière <118798868+anoziere@users.noreply.github.com>
2025-08-26 15:57:03 +0200

    Ensure constants are defined only if undefined
    Added checks to define constants only if they are not already defined.

--- bootstrap.php
+++ bootstrap.php
@@ -3,17 +3,30 @@
 // Here you can override thelia directories constants.
 // Please see vendor/thelia/core/bootstrap.php
 
-define('DS', DIRECTORY_SEPARATOR);
-define ("THELIA_ROOT", __DIR__ . DS);
-define ('THELIA_VENDOR', __DIR__ . DS . 'vendor' . DS);
-define('THELIA_LOCAL_DIR', THELIA_ROOT.'local'.DS);
-define ('THELIA_LIB', THELIA_VENDOR . 'thelia' . DS . 'core' . DS . 'lib' . DS . 'Thelia' . DS);
-if (is_dir(THELIA_VENDOR.'thelia'.DS.'setup'.DS) ) {
-    define('THELIA_SETUP_DIRECTORY', THELIA_VENDOR.'thelia'.DS.'setup'.DS);
-} elseif(is_dir(THELIA_ROOT.'setup'.DS)) {
-    define('THELIA_SETUP_DIRECTORY', THELIA_ROOT.'setup'.DS);
-} else {
-    define('THELIA_SETUP_DIRECTORY', THELIA_LOCAL_DIR.'setup'.DS);
+if(!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+if(!defined('THELIA_ROOT')) {
+    define ("THELIA_ROOT", __DIR__ . DS);
+}
+if(!defined('THELIA_VENDOR')) {
+    define ('THELIA_VENDOR', __DIR__ . DS . 'vendor' . DS);
+}
+if(!defined('THELIA_LOCAL_DIR')) {
+    define('THELIA_LOCAL_DIR', THELIA_ROOT.'local'.DS);
+}
+if(!defined('THELIA_LIB')) {
+    define ('THELIA_LIB', THELIA_VENDOR . 'thelia' . DS . 'core' . DS . 'lib' . DS . 'Thelia' . DS);
+}
+
+if(!defined('THELIA_SETUP_DIRECTORY')) {
+    if (is_dir(THELIA_VENDOR . 'thelia' . DS . 'setup' . DS)) {
+        define('THELIA_SETUP_DIRECTORY', THELIA_VENDOR . 'thelia' . DS . 'setup' . DS);
+    } elseif (is_dir(THELIA_ROOT . 'setup' . DS)) {
+        define('THELIA_SETUP_DIRECTORY', THELIA_ROOT . 'setup' . DS);
+    } else {
+        define('THELIA_SETUP_DIRECTORY', THELIA_LOCAL_DIR . 'setup' . DS);
+    }
 }
 
 // --------------------------------------------------

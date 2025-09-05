c74c21b2c8beded3899d5f541cd7da335b1ce827
Alexandre Nozière <118798868+anoziere@users.noreply.github.com>
2025-09-05 15:52:40 +0200

    Update index.php for better compliance Symfony

--- public/index.php
+++ public/index.php
@@ -1,34 +1,28 @@
 <?php
-/*************************************************************************************/
-/*      This file is part of the Thelia package.                                     */
-/*                                                                                   */
-/*      Copyright (c) OpenStudio                                                     */
-/*      email : dev@thelia.net                                                       */
-/*      web : http://www.thelia.net                                                  */
-/*                                                                                   */
-/*      For the full copyright and license information, please view the LICENSE.txt  */
-/*      file that was distributed with this source code.                             */
-/*************************************************************************************/
 
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\Debug;
-use Thelia\Core\HttpFoundation\Request;
+declare(strict_types=1);
 
-//use Symfony\Component\DependencyInjection;
+/*
+ * This file is part of the Thelia package.
+ * http://www.thelia.net
+ *
+ * (c) OpenStudio <info@thelia.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-$env = 'prod';
-$loader = require __DIR__ . '/../bootstrap.php';
+use App\Kernel;
+use Thelia\Core\HttpFoundation\Request as TheliaRequest;
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+require dirname(__DIR__).'/bootstrap.php';
 
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
+return static function (array $context): Kernel {
+    $thelia = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $request = TheliaRequest::createFromGlobals();
+    $response = $thelia->handle($request);
+    $response->send();
+    $thelia->terminate($request, $response);
 
-    Debug::enable();
-}
-
-$thelia = new App\Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
-$response = $thelia->handle($request);
-$response->send();
-$thelia->terminate($request, $response);
+    return $thelia;
+};
